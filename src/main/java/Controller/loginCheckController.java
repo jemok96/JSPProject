feefff1bea2d5dd0c9b21c14c45a6d7072ec6977
경제목 <jemok9605@naver.com>
2022-10-20 16:08:15 +0900

    test DB loginCheck

--- src/main/java/Controller/loginCheckController.java
+++ src/main/java/Controller/loginCheckController.java
@@ -1,6 +1,11 @@
 package Controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,12 +18,30 @@ import javax.servlet.http.HttpSession;
 public class loginCheckController extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String userId = request.getParameter("userId");
-		String userpw = request.getParameter("userpw");
+		String userId_ = request.getParameter("userId");
+		String userpw_ = request.getParameter("userpw");
 		HttpSession session = request.getSession();
-		session.setAttribute("userId", userId);
-
-		request.getRequestDispatcher("/WEB-INF/view/loginCheck.jsp")
-		.forward(request, response);
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String sql = "SELECT * FROM DONG_USER WHERE ID=?";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url,"scott","tiger");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, userId_);
+			ResultSet rs = st.executeQuery();
+			if(rs.next()) {
+				String userId = rs.getString("ID");
+				String userpw = rs.getString("PASSWORD");
+				if(userId.equals(userId_) &&userpw.equals(userpw_)) {
+					session.setAttribute("userId", userId);
+					response.sendRedirect("/index");;
+				}else {
+					request.getRequestDispatcher("/WEB-INF/view/loginFail.jsp");
+				}
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/Controller/registerFornController.java
+++ src/main/java/Controller/registerFornController.java
@@ -1,10 +1,6 @@
 package Controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
